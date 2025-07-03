d78bfc07ea327896aab7ae83e63a6829abb963c6
Chinnabathini Arogya vamshi <arogyavamshi2002@gmail.com>
2025-07-03 12:01:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -211,13 +211,5 @@ async def health_check():
         "process_pool_active": bool(process_executor)
     }
 
-# if __name__ == "__main__":
-#     import uvicorn
-#     uvicorn.run(
-#         "main:app",
-#         host="0.0.0.0", 
-#         port=8000,
-#         reload=True,
-#         # For production, use multiple workers:
-#         # workers=4
-#     )
+if __name__ == "__main__":
+    uvicorn_run("app:app",host="0.0.0.0", port=8001, reload=True)
