0c946e910a7a6010b7cef10455dbdd2e45e64283
Chinnabathini Arogya vamshi <arogyavamshi2002@gmail.com>
2025-07-08 11:29:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -213,4 +213,4 @@ async def health_check():
     }
 
 if __name__ == "__main__":
-    uvicorn.run("app:app",host="0.0.0.0", port=8001, reload=False)
+    uvicorn.run("app:app",host="0.0.0.0", port=8001)
